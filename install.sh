38496c210c071eb258449fec5b4844bca71dbcbe
cloudDDoSonline <111305645+cloudDDoSonline@users.noreply.github.com>
2022-08-21 00:24:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
-cd
+cd ~
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
 sudo rm /var/lib/dpkg/lock-frontend
 sudo dpkg --configure -a
-cd bot
+cd ~
+cd /workspace/bot/
 sudo apt update
 sudo apt install python3-pip
-cd
-cd bot
+cd ~
+cd /workspace/bot/
 python3 -m venv tutorial-env
 source tutorial-env/bin/activate
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -16,21 +17,21 @@ python3.8 get-pip.py
 sudo apt update
 sudo apt install python3-pip
 pip update python
-cd
-cd bot
+cd ~
+cd /workspace/bot/
 python3.8 -m pip install -r requirements.txt
 sudo apt install vnstat
 sudo service vnstat start
 sudo vnstat -u
 apt install slurm
 apt install screen
-cd
-cd bot
+cd ~
+cd /workspace/bot/
 ./build.sh
-cd
-cd bot
+cd ~
+cd /workspace/bot/
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
-cd
-cd bot
+cd ~
+cd /workspace/bot/
 cd mhddos_proxy
 python3.8 -m pip install -r requirements.txt
