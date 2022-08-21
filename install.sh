3d6d2a5aae7c962eb6bf63d6bdb16654f4bfa867
cloudDDoSonline <111305645+cloudDDoSonline@users.noreply.github.com>
2022-08-21 00:18:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
-cd ~
-cd bot
-python3 -m venv tutorial-env
-source tutorial-env/bin/activate
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install python3.8 -y
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.8
+cd
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
 sudo rm /var/lib/dpkg/lock-frontend
 sudo dpkg --configure -a
+cd bot
+sudo apt update
+sudo apt install python3-pip
+cd
+cd bot
+python3 -m venv tutorial-env
 source tutorial-env/bin/activate
-pip install --upgrade pip
-cd ~
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.8 get-pip.py
+sudo apt update
+sudo apt install python3-pip
+pip update python
+cd
 cd bot
 python3.8 -m pip install -r requirements.txt
 sudo apt install vnstat
@@ -21,12 +24,13 @@ sudo service vnstat start
 sudo vnstat -u
 apt install slurm
 apt install screen
-cd ~
+cd
 cd bot
 ./build.sh
-cd ~
+cd
 cd bot
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
-cd ~
-cd /bot/mhddos_proxy/
+cd
+cd bot
+cd mhddos_proxy
 python3.8 -m pip install -r requirements.txt
