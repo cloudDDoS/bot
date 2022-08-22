2fff15bb48207af299600cbcb57b84235bd1392d
cloudDDoSonline <111305645+cloudDDoSonline@users.noreply.github.com>
2022-08-23 00:07:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 cd ~
-cd /workspace/mhddos_proxy/
+cd /workspace/mhddos_proxy/bot/
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
 sudo rm /var/lib/dpkg/lock-frontend
 sudo dpkg --configure -a
 cd ~
-cd /workspace/mhddos_proxy/
+cd /workspace/mhddos_proxy/bot/
 sudo apt update
 sudo apt install python3-pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -15,7 +15,7 @@ sudo apt update
 sudo apt install python3-pip
 pip update python
 cd ~
-cd /workspace/mhddos_proxy/
+cd /workspace/mhddos_proxy/bot
 python3 -m pip install -r requirements.txt
 sudo apt install vnstat
 sudo service vnstat start
